9fee7bd54933ff0df9d3eaf310586d8bc074d722
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-04-21 14:14:36 +0700

    Initial commit

---
+++ src/test/java/com/praktikumDB/deployment/DeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praktikumDB.deployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
