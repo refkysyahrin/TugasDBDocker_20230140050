1cca97cef1e7ccb6421a5b636bb34f2fc9ead9eb
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-04-21 14:43:30 +0700

    UserRepository.java

--- src/main/java/com/praktikumDB/deployment/repository/UserRepository.java
+++ src/main/java/com/praktikumDB/deployment/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.praktikumDB.deployment.repository;
 
-public interface UserRepository {
+import com.praktikumDB.deployment.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
 }
